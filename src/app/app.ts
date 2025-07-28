247fb56369c9ba547df55f2aa21436da2a45198e
DavidGonzalez-dev <davidfgonzalezc1@gmail.com>
2025-07-28 12:11:15 -0500

    feat: components creation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './components/task-form/task-form';
+import { TaskItemComponent } from './components/task-item/task-item';
+import { TaskListComponent } from './components/task-list/task-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TaskFormComponent, TaskItemComponent, TaskListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo-app');
+  protected readonly title = signal('TodoApp');
 }
